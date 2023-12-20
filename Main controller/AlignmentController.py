3315a253a5658607d1ea6890f12f5b15ac5f8780
ricardosnabel <s1141231@student.windesheim.nl>
2023-12-20 14:31:36 +0100

    Testing

--- Main controller/AlignmentController.py
+++ Main controller/AlignmentController.py
@@ -112,9 +112,11 @@ def move_actuator(data, check):
             write_to_arduino(0)
             write_to_arduino(abs(stepsToTake)) if float(data[XAXISCAM2]) < 0.0 else write_to_arduino(-stepsToTake)'''
     else:
-        diff = abs(float(data[YAXISCAM0])) - abs(float((data[YAXISCAM2]))) / 2
-        data[YAXISCAM0] = float(data[YAXISCAM0]) - diff
-        data[YAXISCAM2] = float(data[YAXISCAM2]) - diff
+        diff = abs(float(data[YAXISCAM0])) - abs(float((data[YAXISCAM2])))
+        if diff > 0:
+            data[YAXISCAM2] = float(data[YAXISCAM2]) - diff
+        else:
+            data[YAXISCAM0] = float(data[YAXISCAM0]) - diff
         stepsToTake = convert_um2steps(convert_pixels2um(data))
         #print("Translation: ", stepsToTake)
         write_to_arduino(stepsToTake[YAXISCAM0] / 2)
