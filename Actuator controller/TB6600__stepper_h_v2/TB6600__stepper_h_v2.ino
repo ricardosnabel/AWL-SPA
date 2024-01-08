460016fa3f9000844d288a2a7337038a6bc0bf01
ricardosnabel <s1141231@student.windesheim.nl>
2024-01-08 16:41:36 +0100

    Update TB6600__stepper_h_v2.ino

--- Actuator controller/TB6600__stepper_h_v2/TB6600__stepper_h_v2.ino
+++ Actuator controller/TB6600__stepper_h_v2/TB6600__stepper_h_v2.ino
@@ -49,22 +49,31 @@ void stepper_innit(){
   steppers.addStepper(stepperX);
 }
 
+void step_direction(long steps, uint8_t dirPin){
+  if (steps > 0)
+    digitalWrite(dirPin, true);
+  else
+    digitalWrite(dirPin, false);
+}
+
 void loop() {
   long positions[3];
   float readMotorY1 = 0.0;
   float readMotorY2 = 0.0;
   float readMotorX = 0.0;
-  uint8_t enaPin = 0;
-  uint8_t dirPin = 0;
 
   if (Serial.available() >= 0){
     positions[Y1] = Serial.readStringUntil(';').toInt();
     positions[Y2] = Serial.readStringUntil(';').toInt();
     positions[X]  = Serial.readStringUntil(';').toInt();
 
-    Serial.println(positions[Y1]);
-    Serial.println(positions[Y2]);
-    Serial.println(positions[X]);
+    step_direction(positions[Y1], dirPinY1);
+    step_direction(positions[Y2], dirPinY2);
+    step_direction(positions[X], dirPinX);
+
+    //Serial.println(positions[Y1]);
+    //Serial.println(positions[Y2]);
+    //Serial.println(positions[X]);
 
     steppers.moveTo(positions);
 
