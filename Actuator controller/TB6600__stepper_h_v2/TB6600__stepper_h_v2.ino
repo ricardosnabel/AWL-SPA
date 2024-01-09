5e620b7547f0fd52efb4ffafc7f062f747f7281c
ricardosnabel <s1141231@student.windesheim.nl>
2024-01-09 15:56:58 +0100

    updated code

--- Actuator controller/TB6600__stepper_h_v2/TB6600__stepper_h_v2.ino
+++ Actuator controller/TB6600__stepper_h_v2/TB6600__stepper_h_v2.ino
@@ -18,7 +18,7 @@ AccelStepper stepperY1(1, pulPinY1, dirPinY1);
 AccelStepper stepperY2(1, pulPinY2, dirPinY2);
 AccelStepper stepperX(1, pulPinX, dirPinX);
 
-MultiStepper steppers;
+//MultiStepper steppers;
 
 void setup() {
   Serial.setTimeout(2);
@@ -34,19 +34,26 @@ void setup() {
   pinMode(enaPinX, OUTPUT);
 
   stepper_innit();
+
   digitalWrite(enaPinY1, true);
   digitalWrite(enaPinY2, true);
   digitalWrite(enaPinX, true);
 }
 
 void stepper_innit(){
-  stepperY1.setMaxSpeed(3200);
-  stepperY2.setMaxSpeed(3200);
   stepperX.setMaxSpeed(3200);
+  stepperX.setAcceleration(400);
+  stepperX.setSpeed(3200);
+  stepperY2.setMaxSpeed(3200);
+  stepperY2.setAcceleration(400);
+  stepperY2.setSpeed(3200);
+  stepperY1.setMaxSpeed(3200);
+  stepperY1.setAcceleration(400);
+  stepperY1.setSpeed(3200);
 
-  steppers.addStepper(stepperY1);
-  steppers.addStepper(stepperY2);
-  steppers.addStepper(stepperX);
+  //steppers.addStepper(stepperY1);
+  //steppers.addStepper(stepperY2);
+  //steppers.addStepper(stepperX);
 }
 
 long step_direction(long steps, uint8_t dirPin){
@@ -54,36 +61,50 @@ long step_direction(long steps, uint8_t dirPin){
     digitalWrite(dirPin, true);
   else
     digitalWrite(dirPin, false);
-  return steps;
+  return abs(steps);
 }
 
 void loop() {
   long positions[3];
 
   if (Serial.available() >= 0){
-    positions[Y1] = step_direction(Serial.readStringUntil(';').toInt(), dirPinY1);
-    positions[Y2] = step_direction(Serial.readStringUntil(';').toInt(), dirPinY2);
-    positions[X]  = step_direction(Serial.readStringUntil(';').toInt(), dirPinX);
+    positions[Y1] = Serial.readStringUntil(';').toInt();
+    positions[Y2] = Serial.readStringUntil(';').toInt();
+    positions[X]  = Serial.readStringUntil(';').toInt();
 
-    //Serial.println(positions[Y1]);
-    //Serial.println(positions[Y2]);
-    //Serial.println(positions[X]);
+    //step_direction(positions[Y1], dirPinY1);
+    //step_direction(positions[Y2], dirPinY2);
+    //step_direction(positions[X], dirPinX);
 
-    steppers.moveTo(positions);
+    //steppers.moveTo(positions);
+    stepperY1.moveTo(positions[Y1]);
+    stepperY2.moveTo(positions[Y2]);
+    stepperX.moveTo(positions[X]);
+
 
     digitalWrite(enaPinY1, false);
     digitalWrite(enaPinY2, false);
     digitalWrite(enaPinX, false);
+    delay(2000);
 
-    steppers.runSpeedToPosition();
+    //steppers.runSpeedToPosition();
+    stepperY1.runToPosition();
 
-    delay(5000);
+    delay(2000);
+    
+    stepperY2.runToPosition();
+
+    delay(2000);
+    
+    stepperX.runToPosition();
+
+    delay(2000);
+
 
     digitalWrite(enaPinY1, true);
     digitalWrite(enaPinY2, true);
     digitalWrite(enaPinX, true);
 
     Serial.println("end");
-
   }
 }
