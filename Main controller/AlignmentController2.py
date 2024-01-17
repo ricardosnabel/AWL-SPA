b27ce20c795aecfac7c967069be40ad87d44a330
ricardosnabel <s1141231@student.windesheim.nl>
2024-01-17 11:39:42 +0100

    Comments placed

--- Main controller/AlignmentController2.py
+++ Main controller/AlignmentController2.py
@@ -12,7 +12,7 @@ This controller use the pySerial package. If there will be no use of any serial 
 Set up a connection:
     Telnet:
     This controller sets a connection between the Omron Vision controller and the CoVi controller. May the IP address of the Omron controller be changed, please change the address in the first element of the 'OMRONCONTROLLER' array. To change the port, use the second element in this array.
-    It is also possible to set up an connection with another device, for this it is necessary to insert the IP address of this device in the first element of the 'EXTERNCONTROLLER' array and uncomment all the line using the 'CONNEXTERN' command. To change the port, use the second element in this array.
+    It is also possible to set up an connection with the transport system and screen printing module, therefore it is necessary to insert the IP addresses in the first element of the 'TRANSPORT' and 'SCREENPRINT' array and uncomment all the lines using the 'CONNTRANSPORT' and 'CONNSCREENPRINT' command. To change the port, use the second element in these arrays.
     To send data, use the sendmsg() function. This function requires the socket of the connection to which the data needs to be transmitted to and the message that will be sent.   
     To receive data, use the receive_data() function. This function requires the socket of the connection from which the data needs to be received.
     For more information, see the documentation of the socket library: https://docs.python.org/3/library/socket.html
@@ -34,9 +34,8 @@ When data is received, this will be converted from pixels to steps. To remain th
 To ensure the safety of the compliant mechanism, the controller checks if the amount of steps is not greater than the steps stored in the variable 'MAXSTEPS'.
 
 Start and stop the controller:
-To be used in production, the controller will start when it receives a message from an external device. Before using this controller, change the variable 'EXTERNRECV' to the expected message the controller will receive. In the state 'waiting for plate' in function run_covi() replace the line 'if runApp:' with the following two lines: 'data = receive_data(CONNEXTERN)' and 'if data[0] == EXTERNRECV:'.
-The CoVi controller is equipped with two buttons, red and green, to be used without receiving a message from an external device. Therefore, in the state 'waiting for plate' in function run_covi(), replace the following two lines 'data = receive_data(CONNEXTERN)' and 'if data[0] == EXTERNRECV:' with the following: 'if runApp':
-These buttons are used to start the controller when this is not used in production.
+To be used in production, the controller will start when it receives a message from the transport system. Before using this controller, change the variable 'TRANSPORTRECV' to the expected message the controller will receive. In the state 'waiting for plate' in function run_covi() add the following two lines: 'data = receive_data(CONNTRANSPORT)' and 'if data[0] == TRANSPORTRECV:'.
+The CoVi controller is equipped with two buttons, red and green, to be used without receiving a message from an external device. Therefore, in the state 'waiting for plate' in function run_covi(), make sure the following two lines 'data = receive_data(CONNEXTERN)' and 'if data[0] == EXTERNRECV:' are deleted or commented.
 Pushing the green button interrupts the controller and will run the function run_covi(). While running, the LED is turned on.
 Pushing the red button interrupts the controller and will stop the function run_covi(). The LED will turn off.
 
@@ -47,7 +46,7 @@ XAXISCAM0 = 0                                                               # In
 YAXISCAM0 = 1                                                               # Index for the measurement value of the Y axis on camera 0
 XAXISCAM2 = 2                                                               # Index for the measurement value of the X axis on camera 2
 YAXISCAM2 = 3                                                               # Index for the measurement value of the Y axis on camera 2
-EXTERNRECV = 'OK\r'                                                         # Message to receive to run the controller
+TRANSPORTRECV = 'OK\r'                                                      # Message to receive to run the controller
 MEASURE = 'M'                                                               # Command to run a measurement on the Omron controller
 LAYOUT = 'DLN 0 1'                                                          # Set layout command for the Omron controller 
 PIXELSIZE = 9.922                                                           # Size of a single pixel in micrometers
@@ -57,18 +56,24 @@ LED = 11                                                                    # GP
 REDBUTTON = 16                                                              # GPIO pin for the red button
 GREENBUTTON = 15                                                            # GPIO pin for the green button
 CONNOMRON = socket.socket(socket.AF_INET, socket.SOCK_STREAM)               # Create a socket for the connection with the Omron controller
-CONNEXTERN = socket.socket(socket.AF_INET, socket.SOCK_STREAM)              # Create a socket for the connection with an external device
+CONNTRANSPORT = socket.socket(socket.AF_INET, socket.SOCK_STREAM)           # Create a socket for the connection with an external device
+CONNSCREENPRINT = socket.socket(socket.AF_INET, socket.SOCK_STREAM)         # Create a socket for the connection with an external device
 SERIAL = serial.Serial(port='/dev/ttyUSB0', baudrate=2000000, timeout=.1)   # Creates a serial connection
 OMRONCONTROLLER = ['10.5.5.100', 9876]                                      # Sets the IP address and port of the Omron controller
-EXTERNCONTROLLER = ['127.0.0.1', 0]                                         # Sets the IP address and port of an external device
+TRANSPORT = ['127.0.0.1', 0]                                                # Sets the IP address and port of an external device
+SCREENPRINT = ['127.0.0.1', 0]                                              # Sets the IP address and port of an external device
 runApp = False                                                              # Boolean variable to start (true) and stop (false) the controller
 
+#####################################################
+# Initialization of the connections and the GPIO pins
 def conn_init():
     CONNOMRON.settimeout(1)
     CONNOMRON.connect((OMRONCONTROLLER[0], OMRONCONTROLLER[1]))
     sendmsg(CONNOMRON, LAYOUT)
-    #CONNEXTERN.settimeout(1)
-    #CONNEXTERN.connect((EXTERNCONTROLLER[0], EXTERNCONTROLLER[1]))
+    #CONNTRANSPORT.settimeout(1)
+    #CONNTRANSPORT.connect((TRANSPORT[0], TRANSPORT[1]))
+    #CONNSCREENPRINT.settimeout(1)
+    #CONNSCREENPRINT.connect((SCREENPRINT[0], SCREENPRINT[1]))
 
 def GPIO_init():
     GPIO.setmode(GPIO.BOARD)
@@ -77,7 +82,10 @@ def GPIO_init():
     GPIO.setup(GREENBUTTON, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
     GPIO.add_event_detect(REDBUTTON, GPIO.RISING, callback=handle_buttonINT, bouncetime=1000)       # handle_redbutton
     GPIO.add_event_detect(GREENBUTTON, GPIO.RISING, callback=handle_buttonINT, bouncetime=1000)     # handle_greenbutton
+#####################################################
 
+#####################################################
+# Functions for sending and receiving data
 def sendmsg(sock, message):
     sock.send(message.encode())
 
@@ -102,7 +110,10 @@ def read_serial():
         if not recv:
             break
         print(recv)
+#####################################################
 
+#####################################################
+# Functions for converting pixels to steps
 def convert_pixels2steps(data):
     if isinstance(data, float):
         distInUm = PIXELSIZE * data
@@ -119,7 +130,10 @@ def maxsteps_check(steps):
     if steps > MAXSTEPS or steps < -MAXSTEPS:
         steps = 0
     return steps
+#####################################################
 
+#####################################################
+# Function to calculate the movement
 def move_actuator(data):
     write_to_serial("start")
     YDiff = abs(float(data[YAXISCAM0]) - float(data[YAXISCAM2]))
@@ -141,7 +155,10 @@ def move_actuator(data):
         write_to_serial(0)
         write_to_serial(stepsToTake[XAXISCAM0])
     write_to_serial("end")
+#####################################################
 
+#####################################################
+# Interrupt handler for the buttons
 def handle_buttonINT(channel):
     global runApp
     if channel == REDBUTTON:
@@ -152,26 +169,16 @@ def handle_buttonINT(channel):
         runApp = True
         print("green")
         GPIO.output(11, 1)
+#####################################################
 
-def handle_greenbutton(channel):
-    global runApp
-    runApp = True
-    print("green")
-    GPIO.output(11, 1)
-
-def handle_redbutton(channel):
-    global runApp
-    runApp = False
-    print("red")
-    GPIO.output(11, 0)
-
+#####################################################
+# The application
 def run_covi(status):
     global runApp
     while True:
         while runApp:
             match status:
                 case 'waiting for plate':
-                    #if runApp:
                     status = 'plate arrived'
                 case 'plate arrived':
                     sendmsg(CONNOMRON, MEASURE)
@@ -188,13 +195,15 @@ def run_covi(status):
                     else:
                         move_actuator(data[MEASUREDDATA])
                 case 'aligned':
-                    #sendmsg(CONNEXTERN, 'OK')
-                    status = 'wait for external module'
-                case 'wait for external module':
-                    #if receive_data(CONNEXTERN) == 'OK\r':
+                    #sendmsg(CONNSCREENPRINT, 'OK')
+                    status = 'wait for screen print'
+                case 'wait for screen print':
+                    #if receive_data(CONNSCREENPRINT) == 'OK\r':
                     status = 'return to neutral'
                 case 'return to neutral':
+                    # insert code to return the actuators to their neutral position
                     status = 'waiting for plate'
+#####################################################
 
 if __name__ == '__main__':
     GPIO_init()
@@ -205,6 +214,7 @@ if __name__ == '__main__':
     except KeyboardInterrupt:
         GPIO.cleanup()
         CONNOMRON.close()
-        #CONNEXTERN.close()
+        #CONNTRANSPORT.close()
+        #CONNSCREENPRINT.close()
         SERIAL.close()
     GPIO.cleanup()
