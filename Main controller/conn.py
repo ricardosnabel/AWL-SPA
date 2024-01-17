4a9ac71e3d470ab0ee63d594d30028363308308c
ricardosnabel <s1141231@student.windesheim.nl>
2024-01-17 15:53:54 +0100

    Update conn.py

--- Main controller/conn.py
+++ Main controller/conn.py
@@ -33,15 +33,16 @@ def sendmsg(sock, message):
     sock.send(message.encode())
 
 def receive_data(sock):
-    fragments = []
+    
     try:
         data = sock.recv(1024).decode().replace(" ", "")
         print(data)
         re.split("\r |, ", data)
+        fragments = []
         fragments.append(data.split(","))
         return fragments
     except TimeoutError:
-        return fragments
+        return "ERR"
 
 def write_to_serial(data):
     time.sleep(2)
